60b0c2ff45ba3ac9c0a0253b974a610c614e8fdb
tgustafson2 <gus.duckfan@gmail.com>
2023-10-30 17:09:21 -0700

    Added routes for upserting and deleting meal choice

--- Controllers/MealController.cs
+++ Controllers/MealController.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using Dapper;
 using MealPlannerBackend.Data;
 using MealPlannerBackend.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -11,5 +12,45 @@ namespace MealPlannerBackend.Controllers{
     [Route("[controller]")]
     public class MealController : ControllerBase{
         
+        private readonly DatabaseContext _dapper;
+        public MealController(IConfiguration config){
+            _dapper = new DatabaseContext(config);
+        }
+
+
+
+        [HttpPut("UpsertMeal")]
+        public IActionResult UpsertMeal(Meal meal){
+            string sql = @"EXEC MealPlanning.spMealChoiceUpsert
+                @MealDate = @MealDateParam,
+                @UserId = @UserIdParam,
+                @RecipeId = @RecipeIdParam";//@Id
+            DynamicParameters sqlParams = new DynamicParameters();
+            sqlParams.Add("@MealDateParam", meal.MealDate, DbType.DateTime);
+            sqlParams.Add("@UserIdParam", this.User.FindFirst("userId")?.Value, DbType.Int32);
+            sqlParams.Add("@RecipeIdParam", meal.RecipeId, DbType.Int32);
+            if(meal.Id>0){
+                sql+=", @Id = @IdParam";
+                sqlParams.Add("IdParam", meal.Id, DbType.Int32);
+            }
+            if(_dapper.ExecuteSqlWithParameters(sql,sqlParams)){
+                return Ok();
+            }
+            throw new Exception("Failed to upsert meal");
+        }
+        [HttpDelete("DeleteMeal/{Id}")]
+        public IActionResult DeleteMeal(int Id){
+            string sql = @"EXEC MealPlanning.spMealChoiceDelete
+                @Id = @IdParam,
+                @UserId = @UserIdParam";
+            DynamicParameters sqlParams = new DynamicParameters();
+            sqlParams.Add("@IdParam", Id, DbType.Int32);
+            sqlParams.Add("@UserIdParam", this.User.FindFirst("userId")?.Value, DbType.Int32);
+
+            if(_dapper.ExecuteSqlWithParameters(sql,sqlParams)){
+                return Ok();
+            }
+            throw new Exception("Failed to delete meal");
+        }
     }
 }
